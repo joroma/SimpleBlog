004d74869dcaa7975826fa9e04b93b3361c41100
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-01 16:49:32 +0800

    Update to data access class

--- SimpleBlog.Core/BlogRepository.cs
+++ SimpleBlog.Core/BlogRepository.cs
@@ -1,12 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Entity;
+using SimpleBlog.Core.Mappings;
+using SimpleBlog.Core.Objects;
+
 
 namespace SimpleBlog.Core
 {
     public class BlogRepository : IBlogRepository
     {
+        private readonly SimpleBlogContext db = new SimpleBlogContext();
+        
+        public IList<Post> Posts (int pageNo, int pageSize)
+        {
+            var posts = db.Posts
+                            .Where(p => p.Published)
+                            .OrderByDescending(p => p.PostedOn)
+                            .Skip(pageNo * pageSize)
+                            .Take(pageSize)
+                            .Include(p => p.Category)
+                            .ToList();
+            var PostIDs = posts.Select(p => p.ID).ToList();
+
+            return db.Posts.Where(p => PostIDs.Contains(p.ID))
+                            .OrderByDescending(p => p.PostedOn)
+                            .Include(p => p.Tags)
+                            .ToList();
+        }
+
+        public int TotalPosts()
+        {
+            return db.Posts.Where(p => p.Published).Count();
+        }         
     }
 }
