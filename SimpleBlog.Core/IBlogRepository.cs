912dd7db386c98ee13563739e4915ea61ae52bfc
Joro Maglalang <johnroger.maglalang@gmail.com>
2016-11-01 13:35:34 +0800

    Add data access class

---
+++ SimpleBlog.Core/IBlogRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleBlog.Core.Objects;
+
+namespace SimpleBlog.Core
+{
+    public interface IBlogRepository
+    {
+        IList<Post> Posts(int PageNo, int PageSize);
+        int TotalPosts();
+    }
+}
