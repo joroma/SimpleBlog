ffc83339dede0cf09a3bf9ee43e4ee40c88d6f10
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-02 19:10:56 +0800

    Install built-in Unity initializer

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -14,9 +14,19 @@ namespace SimpleBlog
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Category",
+                url: "Category/{category}",
+                defaults: new { controller = "Blog", action = "Category" });
+
+            routes.MapRoute(
+                name: "Tag",
+                url: "Tag/{tag}",
+                defaults: new { controller = "Blog", action = "Tag" });
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Blog", action = "Posts", id = UrlParameter.Optional }
             );
         }
     }
