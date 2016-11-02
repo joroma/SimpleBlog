a3cbf8d11cdf178dd9484bda1eacc83abf68cf53
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-02 19:13:48 +0800

    Mock Unity Container

---
+++ SimpleBlog/DependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Practices.Unity;
+using SimpleBlog.Core;
+
+namespace SimpleBlog
+{
+    public class DependencyContainer
+    {
+        public static void RegisterElements(IUnityContainer container)
+        {
+            //BlogRepository blogRepository = new BlogRepository();
+            //container.RegisterInstance(blogRepository);
+            //container.RegisterType<IBlogRepository, BlogRepository>();
+        }
+    }
+}
