4f65779a7fe50abcd489ec62c96b6a4aaa156aee
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-02 13:06:22 +0800

    add model

---
+++ SimpleBlog/Models/ListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleBlog.Core;
+using SimpleBlog.Core.Objects;
+
+namespace SimpleBlog.Models
+{
+    public class ListViewModel
+    {
+
+        public ListViewModel(IBlogRepository _blogRepository, int p)
+        {
+            Posts = _blogRepository.Posts(p - 1, 10);
+            TotalPosts = _blogRepository.TotalPosts();
+        }
+
+        public IList<Post> Posts { get; private set; }
+        public int TotalPosts { get; private set; }
+    }
+}
