3b5181443aeef496c334fc47e0829d49c98ddd85
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-02 13:06:42 +0800

    Add controller and action

---
+++ SimpleBlog/Controllers/BlogController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleBlog.Core;
+using SimpleBlog.Models;
+
+
+namespace SimpleBlog.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogRepository _blogRepository;
+
+        public BlogController(IBlogRepository blogRepository)
+        {
+            _blogRepository = blogRepository;
+        }
+
+        public ViewResult Posts(int p = 1)
+        {
+            // pick latest 10 posts
+            var viewModel = new ListViewModel(_blogRepository, p);
+
+            ViewBag.Title = "Latest Posts";
+
+            return View("List", viewModel);
+        }
+    }
+}
