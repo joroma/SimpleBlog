09faa55789e60654c1cf8eb241d2a37a64db3757
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-02 14:41:11 +0800

    Add view helper classes

---
+++ simpleblog/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using SimpleBlog.Core.Objects;
+using System.Web.Mvc;
+
+
+namespace SimpleBlog
+{
+    public static class Extensions
+    {
+        public static string ToConfigLocalTime(this DateTime utcDT)
+        {
+            var istTZ = TimeZoneInfo.FindSystemTimeZoneById(ConfigurationManager.AppSettings["Timezone"]);
+            return String.Format("{0} ({1})", TimeZoneInfo.ConvertTimeFromUtc(utcDT, istTZ).ToShortDateString(), ConfigurationManager.AppSettings["TimezoneAbbr"]);
+        }
+    }
+}
