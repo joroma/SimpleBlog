731ccd1e8a154992a1b928f8a0c430b78853ebee
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-04 09:27:44 +0800

    Add viewmodel for sidebar

---
+++ SimpleBlog/Models/WidgetViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SimpleBlog.Core;
+using SimpleBlog.Core.Objects;
+
+
+namespace SimpleBlog.Models
+{
+    public class WidgetViewModel
+    {
+        public WidgetViewModel(IBlogRepository blogRepository)
+        {
+            Categories = blogRepository.Categories();
+        }
+
+        public IList<Category> Categories { get; private set; }
+    }
+}
