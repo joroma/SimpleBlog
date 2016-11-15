f16ca6e19dfaee7f95aff416659370a6ab197aed
JoRo Maglalang <johnroger.maglalang@gmail.com>
2016-11-15 18:32:33 +0800

    Add Admin Controller
    Add admin controller for login

---
+++ SImpleblog/Controllers/AdminController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleBlog.Models;
+using System.Web.Security;
+
+
+namespace SimpleBlog.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+
+        [AllowAnonymous]
+        public ActionResult Login(string returnUrl)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                if (!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                    return Redirect(returnUrl);
+
+                return RedirectToAction("Manage");
+            }
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost, AllowAnonymous, ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model, string returnUrl)
+        {
+            if(ModelState.IsValid)
+            {
+                if(Membership.ValidateUser(model.UserName, model.Password))
+                {                    
+                    FormsAuthentication.SetAuthCookie(model.UserName, false);
+
+                    if(!String.IsNullOrEmpty(returnUrl) && Url.IsLocalUrl(returnUrl))
+                        return Redirect(returnUrl);
+
+                    return RedirectToAction("Manage");
+                }
+
+                ModelState.AddModelError("", "The user name of password provided is incorrect");
+            }
+
+            return View();
+
+        }
+
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
